79730187b81fef5315e43205f237792bd9c8bf7d
king199025 <korol_dima@list.ru>
2016-02-05 15:12:49 +0300

    456

--- controllers/Office.php
+++ controllers/Office.php
@@ -13,7 +13,7 @@ class Office extends Controller
         $user = new User();
         $user->find()->where(['vk_id' => $this->app->cookie->get('vk_id')])->one();
         $this->app->geo->ip = $this->app->routing->getRealIpAddr();
-        echo $this->app->forms->dropDownList('test', 1, ['Яблоко', 'Груша'], ['class'=>'myClass', 'id'=>'myId', 'prompt'=>'Фрукты']);
+        echo $this->app->forms->dropDownList('test', 2, ['Яблоко', 'Груша'], ['class'=>'myClass', 'id'=>'myId', 'prompt'=>'Фрукты']);
         $geo = $this->app->geo->get_value();
         $this->app->debug->prn($this->app->routing->getRealIpAddr());
         $this->app->parser->render('index', [
--- lib/Forms.php
+++ lib/Forms.php
@@ -5,21 +5,27 @@ class Forms
 {
 
     public function dropDownList($name, $selected, $data, $options = false){
-        $op = '';
-        if($options){
-            foreach($options as $k => $v){
-                $op .= "$k = '$v' ";
-            }
-        }
         $d = '';
+        if($options['prompt']){
+            $d .= "<option selected >".$options['prompt']."</option>";
+        }
         foreach($data as $k => $v){
-            if($k == $selected){
+
+            if($k == $selected && $selected != 0){
                 $d .= "<option selected value='$k'>$v</option>";
             }
             else {
                 $d .= "<option value='$k'>$v</option>";
             }
         }
+
+        $op = '';
+        if($options){
+            foreach($options as $k => $v){
+                $op .= "$k = '$v' ";
+            }
+        }
+
         return "<select name='$name' $op>$d</select>";
     }
 
